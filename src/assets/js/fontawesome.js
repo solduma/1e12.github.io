a7ac23c013e522e89f772f75fec20a1546c56c31
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2022-08-16 16:31:07 +0900

    White Themed

---
+++ src/assets/js/fontawesome.js
@@ -0,0 +1,31 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import {
+  faGraduationCap,
+  faBriefcase,
+  faGear,
+  faArrowTrendUp,
+  faCalculator,
+  faChartSimple,
+  faRobot,
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+  faFaceSmile,
+  
+} from '@fortawesome/free-regular-svg-icons'
+
+import {
+} from '@fortawesome/free-brands-svg-icons'
+
+
+library.add(
+  faFaceSmile,
+  faGraduationCap,
+  faBriefcase,
+  faGear,
+  faArrowTrendUp,
+  faCalculator,
+  faChartSimple,
+  faRobot,
+)
