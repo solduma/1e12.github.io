e5fa6df774d4a9b228fe5809139b37c888f64714
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2022-08-19 01:16:28 +0900

    Skills Added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,9 @@
   <div class="viewWrapper">
     <h2>Education</h2>
     <div class="aboutWrapper">
-      <a href="/"><img src="../assets/img/chief.png" alt="Illinois Chief" /></a>
+      <a href="/"
+        ><img src="../assets/img/chief.png" alt="Illinois Chief Logo"
+      /></a>
       <ul>
         <li><b>University of Illinois at Urbana-Champaign</b></li>
         <br />
@@ -18,19 +20,322 @@
     <div class="aboutWrapper">
       <ul>
         <li>2021-Current</li>
-        <li>2017-2021</li>
-        <li>2014-2017</li>
-        <li>2012-2013</li>
       </ul>
       <ul>
         <li><b>HYUNDAI CAPITAL SERVICES – SENIOR DATA SCIENTIST </b></li>
-        <li><b>EY CONSULTING – SENIOR CONSULTANT, DATA &amp; ANALYTICS</b></li>
-        <li><b>HANWHA CHEMICAL CO. – MANAGER, FIANCIAL ACCOUNTING</b></li>
+        <li>GTM-GA4-BigQuery Implementation</li>
+        <li>Recommender System for Banner Ads on 3rd-Party Sites</li>
+      </ul>
+    </div>
+    <div class="aboutWrapper">
+      <ul>
+        <li>2017-2021</li>
+      </ul>
+      <ul>
         <li>
-          <b
-            >GREEN LIGHT TRADING LLC – DERIVATIVE TRADER, HIGH-FREQUENCY
-            TRADING</b
-          >
+          <b>EY CONSULTING – SENIOR CONSULTANT, DATA &amp; ANALYTICS</b>
+        </li>
+        <li>Emart – Demand Forecasting</li>
+        <li>NH Investment and Securities – Personalized Product and Service</li>
+        <li>
+          Samsung Electronics – Semi-Realtime Monitoring of Global Sellouts
+        </li>
+        <li>Samsung Electronics – GIS Analysis for Market Expansion</li>
+        <li>Samsung Electronics – Semi-Realtime Dashboard for SCM</li>
+      </ul>
+    </div>
+    <div class="aboutWrapper">
+      <ul>
+        <li>2014-2017</li>
+      </ul>
+      <ul>
+        <li><b>HANWHA CHEMICAL CO. – MANAGER, FIANCIAL ACCOUNTING</b></li>
+        <li>Automation of Financial Statement Disclosure</li>
+        <li>Development of Fair-value Monitoring System</li>
+      </ul>
+    </div>
+    <div class="aboutWrapper">
+      <ul>
+        <li>2012-2013</li>
+      </ul>
+      <ul>
+        <li><b>GREEN LIGHT TRADING LLC – HIGH-FREQUENCY TRADER</b></li>
+        <li>Data Mining of Trading Log</li>
+        <li>Hyperparameter Tuning for HFT System</li>
+      </ul>
+    </div>
+    <hr />
+    <br />
+    <h2>Skills</h2>
+    <div class="skillWrapper">
+      <ul>
+        <li><b>Languages</b></li>
+      </ul>
+      <ul>
+        <li>
+          <img
+            src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54&style=flat"
+            alt="Python Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white&style=flat"
+            alt="Java Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/r-%23276DC3.svg?style=for-the-badge&logo=r&logoColor=white&style=flat"
+            alt="R Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white&style=flat"
+            alt="C# Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E&style=flat"
+            alt="Javascript Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/shell_script-%23121011.svg?style=for-the-badge&logo=gnu-bash&logoColor=white&style=flat"
+            alt="Shell Script Badge"
+          />
+        </li>
+      </ul>
+    </div>
+    <div class="skillWrapper">
+      <ul>
+        <li><b>ML/DL</b></li>
+      </ul>
+      <ul>
+        <li>
+          <img
+            src="https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white&style=flat"
+            alt="Numpy Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white&style=flat"
+            alt="Pandas Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&logo=scikit-learn&logoColor=white&style=flat"
+            alt="Scikit-Learn Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/SciPy-%230C55A5.svg?style=for-the-badge&logo=scipy&logoColor=%white&style=flat"
+            alt="Scipy Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=for-the-badge&logo=TensorFlow&logoColor=white&style=flat"
+            alt="Tensorflow Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/Keras-%23D00000.svg?style=for-the-badge&logo=Keras&logoColor=white&style=flat"
+            alt="Keras Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/mlflow-%23d9ead3.svg?style=for-the-badge&logo=numpy&logoColor=blue&style=flat"
+            alt="MLFlow Badge"
+          />
+        </li>
+      </ul>
+    </div>
+    <div class="skillWrapper">
+      <ul>
+        <li><b>Visualization</b></li>
+      </ul>
+      <ul>
+        <li>
+          <img
+            src="https://img.shields.io/badge/Plotly-%233F4F75.svg?style=for-the-badge&logo=plotly&logoColor=white&style=flat"
+            alt="Plotly Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/-Tableau-E97627?logo=tableau&logoColor=white&style=flat"
+            alt="Tableau Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/-Spotfire-40AEF0?logo=spotfire&logoColor=white&style=flat"
+            alt="Spotfire Badge"
+          />
+        </li>
+      </ul>
+      </div>
+    <div class="skillWrapper">
+      <ul>
+        <li><b>WebDev</b></li>
+      </ul>
+      <ul>
+        <li>
+          <img
+            src="https://img.shields.io/badge/vuejs-%2335495e.svg?style=for-the-badge&logo=vuedotjs&logoColor=%234FC08D&style=flat"
+            alt="Vue Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB&style=flat"
+            alt="Express Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white&style=flat"
+            alt="Node Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white&style=flat"
+            alt="Flask Badge"
+          />
+        </li>
+      </ul>
+    </div>
+    <div class="skillWrapper">
+      <ul>
+        <li><b>SQL</b></li>
+      </ul>
+      <ul>
+        <li>
+          <img
+            src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white&style=flat"
+            alt="Postgres SQL Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white&style=flat"
+            alt="MySQL Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/sqlite-%2307405e.svg?style=for-the-badge&logo=sqlite&logoColor=white&style=flat"
+            alt="SQLite Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/MariaDB-003545?style=for-the-badge&logo=mariadb&logoColor=white&style=flat"
+            alt="MariaDB Badge"
+          />
+        </li>
+      </ul>
+    </div>
+    <div class="skillWrapper">
+      <ul>
+        <li><b>NoSQL</b></li>
+      </ul>
+      <ul>
+        <li>
+          <img
+            src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white&style=flat"
+            alt="MongoDB Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/-ElasticSearch-005571?style=for-the-badge&logo=elasticsearch&style=flat"
+            alt="ElasticSearch Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/BigQuery-%234285F4.svg?style=for-the-badge&logo=google-cloud&logoColor=white&style=flat"
+            alt="BigQuery Badge"
+          />
+        </li>
+      </ul>
+    </div>
+    <div class="skillWrapper">
+      <ul>
+        <li><b>CI/CD</b></li>
+      </ul>
+      <ul>
+        <li>
+          <img
+            src="https://img.shields.io/badge/github%20actions-%232671E5.svg?style=for-the-badge&logo=githubactions&logoColor=white&style=flat"
+            alt="Github Action Badge"
+          />
+        </li>
+      </ul>
+    </div>
+    <div class="skillWrapper">
+      <ul>
+        <li><b>Etc</b></li>
+      </ul>
+      <ul>
+        <li>
+          <img
+            src="https://img.shields.io/badge/-GA4-61DAFB?logo=google-analytics&logoColor=white&style=flat"
+            alt="GA4 Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/-GTM-246FDB?logo=google-tag-manager&logoColor=white&style=flat"
+            alt="GTM Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white&style=flat"
+            alt="Docker Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/jira-%230A0FFF.svg?style=for-the-badge&logo=jira&logoColor=white&style=flat"
+            alt="JIRA Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/-Swagger-%23Clojure?style=for-the-badge&logo=swagger&logoColor=white&style=flat"
+            alt="Swagger Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/-selenium-%43B02A?style=for-the-badge&logo=selenium&logoColor=white&style=flat"
+            alt="Selenium Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white&style=flat"
+            alt="Github Badge"
+          />
+        </li>
+        <li>
+          <img
+            src="https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white&style=flat"
+            alt="AWS Badge"
+          />
         </li>
       </ul>
     </div>
@@ -52,12 +357,26 @@ hr {
 
 .aboutWrapper {
   display: flex;
-  padding: 20px 0px;
+  padding: 10px 0px;
 }
 
-.aboutWrapper > a > img, ul:first-child {
+.aboutWrapper > a > img,
+ul:first-child {
   width: 120px;
   margin-right: 20px;
 }
 
+.skillWrapper {
+  display: flex;
+  padding: 10px 0px;
+}
+
+.skillWrapper > ul {
+  display: flex;
+  flex-flow: row-wrap;
+}
+
+.skillWrapper > ul > li {
+  margin-right: 3px;
+}
 </style>
