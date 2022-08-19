ba2255c7dbf53c1d570b607d5303d75f50ad4263
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2022-08-19 12:06:12 +0900

    Enabled hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,37 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "home",
-    component: () => HomeView,
+    path: '/',
+    name: 'home',
+    component: () => HomeView
   },
   {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/AboutView.vue"),
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
   },
   {
-    path: "/project",
-    name: "project",
-    component: () => import("../views/ProjectView.vue"),
+    path: '/project',
+    name: 'project',
+    component: () => import('../views/ProjectView.vue')
   },
   {
-    path: "/blog",
-    name: "blog",
-    component: () => import("../views/BlogView.vue"),
+    path: '/blog',
+    name: 'blog',
+    component: () => import('../views/BlogView.vue')
   },
   {
-    path: "/momo",
-    name: "momo",
-    component: () => import("../views/MomoView.vue"),
+    path: '/momo',
+    name: 'momo',
+    component: () => import('../views/MomoView.vue')
   },
-];
+]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes: routes,
-});
+  history: createWebHistory(),
+  routes: routes
+})
 
-export default router;
+export default router
