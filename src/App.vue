32373bd135aaab1fd9b19c1ef75d16cd3a03d4f4
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2022-08-19 11:02:15 +0900

    Added 404 Redirection

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ export default {
   data() {
     return {
       isOnMobile: false,
+      basename: "/",
     };
   },
   mounted () {
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,37 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: () => import('../views/HomeView.vue')
+    path: "/",
+    name: "home",
+    component: () => HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue'),
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
   },
   {
-    path: '/project',
-    name: 'project',
-    component: () => import('../views/ProjectView.vue')
+    path: "/project",
+    name: "project",
+    component: () => import("../views/ProjectView.vue"),
   },
   {
-    path: '/blog',
-    name: 'blog',
-    component: () => import('../views/BlogView.vue')
+    path: "/blog",
+    name: "blog",
+    component: () => import("../views/BlogView.vue"),
   },
   {
-    path: '/momo',
-    name: 'momo',
-    component: () => import('../views/MomoView.vue')
+    path: "/momo",
+    name: "momo",
+    component: () => import("../views/MomoView.vue"),
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: routes
-})
+  routes: routes,
+});
 
-export default router
+export default router;
