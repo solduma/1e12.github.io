547d82e8b843467d891d7a61eafd6a70017bae26
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2022-08-19 15:27:15 +0900

    CI/CD Enabled

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/solduma.github.io/',
+  publicPath: '/',
   outputDir: 'dist',
   transpileDependencies: true,
 })
